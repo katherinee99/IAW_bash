d4f6e251f1973194c14dff64be509acf4558a666
katherinee99 <92717339+katherinee99@users.noreply.github.com>
2021-11-15 12:49:29 +0100

    Update Pruebas.sh

--- Pruebas.sh
+++ Pruebas.sh
@@ -24,3 +24,16 @@ clear
 	else 
 	    echo salir
 	fi
+	
+#!/bin/bash
+#Crear usuario
+read -p 'Nombre de usuario: ' nombre
+read -p 'Contraseña' password
+echo $nombre 
+echo $password
+
+useradd -p $password $nombre
+mkdir /var/www/$nombre/{web,blog,files}
+chown root:root /var/www/$nombre
+chown $nombre:$nombre /var/www/$nombre/*
+chmod -R 755 /var/www/ 
